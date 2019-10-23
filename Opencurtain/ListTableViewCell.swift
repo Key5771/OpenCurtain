36665c096ea61db25bfdb0835509a43d01f8d055
Key5771 <ksj57715@hanmail.net>
2019-10-23 16:38:59 +0900

    게시판 리스트 UI 구현

---
+++ Opencurtain/ListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListTableViewCell.swift
+//  Opencurtain
+//
+//  Created by 김기현 on 2019/10/23.
+//  Copyright © 2019 김기현. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    @IBOutlet weak var jnuLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Opencurtain/ListViewController.swift
+++ Opencurtain/ListViewController.swift
@@ -7,16 +7,52 @@
 //
 
 import UIKit
+import SideMenu
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var sideButton: UIBarButtonItem!
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        listTableView.delegate = self
+        listTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listTableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as! ListTableViewCell
+        
+        cell.jnuLabel.text = "제주대학교"
+        cell.nameLabel.text = "익명"
+        cell.timestampLabel.text = "2019년 10월 23일"
+        cell.contentLabel.text = "금융권 준비하시는 분 계신가요? 이번에 졸업해서 준비를 하려는데 여러모로 걱정이 많네요 ㅠㅠ ncs 준비는 어떻게 하시는지, 그 외 다른 부분은 어떤거 준…"
+        
+        return cell
+    }
+    
+    
+    
+    @IBAction func sideButtonClick(_ sender: Any) {
+//        let viewController: SideMenuNavigationController = self.storyboard!.instantiateViewController(withIdentifier: "sideMenuView") as! SideMenuNavigationController
+//        viewController.presentationStyle = .viewSlideOutMenuIn
+//        self.present(viewController, animated: true, completion: nil)
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "board")
+        let menu = SideMenuNavigationController(rootViewController: vc)
+        menu.leftSide = true
+        menu.statusBarEndAlpha = 0
+        menu.navigationBar.backgroundColor = UIColor(displayP3Red: 255/255, green: 244/255, blue: 211/255, alpha: 1)
+        self.present(menu, animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
