d3816d201749837c399d4dd0db5f7d02258140a2
Key5771 <ksj57715@hanmail.net>
2019-10-27 14:16:02 +0900

    iPhone 8 기준 UI 변경

--- Opencurtain/API.swift
+++ Opencurtain/API.swift
@@ -14,6 +14,7 @@ import Alamofire
 class NetworkRequest {
     let baseURL = "http://opencurtain.run.goorm.io"
     
+    
     enum API: String {
         case users = "/users"
         case subscribes = "/subscribes"
@@ -24,6 +25,7 @@ class NetworkRequest {
         case posts = "/posts"
         case comments = "/comments"
     }
+
     
     
     
--- Podfile
+++ Podfile
@@ -5,8 +5,9 @@ target 'Opencurtain' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
+pod 'Alamofire', '~> 4.7'
 pod 'SDWebImage'
-pod 'AlamofireObjectMapper'
+pod 'AlamofireObjectMapper', '~> 5.2'
 pod 'SideMenu'
   # Pods for Opencurtain
 
